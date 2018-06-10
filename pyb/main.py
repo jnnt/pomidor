3f2635f274806adadb7307a70f00448a57280bfd
Joanna Tustanowska <github@jnnt.net>
2018-06-10 17:07:15 +0200

    Tweak servo speed and angles

--- pyb/main.py
+++ pyb/main.py
@@ -6,8 +6,9 @@ from pyb import (
 
 SLEEP = 0.1
 
-UP = 90
-DOWN = -90
+UP = -90
+DOWN = 0
+SPEED = 1000
 
 servo = Servo(1)
 servo.angle(30)
@@ -18,10 +19,10 @@ while True:
     if p.any():
         command = p.readline().strip()
         if command == b'up':
-            servo.angle(UP)
+            servo.angle(UP, SPEED)
             p.write(b'up-ok\n')
         elif command == b'down':
-            servo.angle(DOWN)
+            servo.angle(DOWN, SPEED)
             p.write(b'down-ok\n')
         else:
             p.write(command + b'\n')
